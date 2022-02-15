8543bd9bf80c3574fce2f2ae5b1774a0e7363dfb
SFuwka <Alexjavac@yandex.ru>
2022-02-15 22:39:26 +0300

    replace deprecated field 'length' with durationInSeconds

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://apollo-tutorial-s.herokuapp.com/',
+  uri: process.env.NODE_ENV === 'production' ? process.env.REACT_APP_SERVER_URI : 'http://localhost:4000',
   cache: new InMemoryCache(),
 });
 
